66899c03ff4919dfaec50f184d5e88afe404364c
JeffreyDinackus <jeffreydinackus@proton.me>
2024-02-22 18:45:29 -0500

    adding navbar and footer

---
+++ src/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Navbar() {
+
+
+  // get props from app
+
+
+  // display conditionally header or footer with specific info from app
+
+  // style to look gud
+
+
+
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
