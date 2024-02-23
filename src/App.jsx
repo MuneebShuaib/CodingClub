1b37ee0426666aa91c80ae6c91eced3e2266f2cc
JeffreyDinackus <jeffreydinackus@proton.me>
2024-02-22 19:08:54 -0500

    added a neat button we can use

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,28 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+//this is for some animations
+import 'animate.css';
+
+
+import Navbar from './Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(500)
 
 
+  function animate() {
+    setCount(count + 10)
 
+    const element = document.querySelector('.the_button');
+    element.classList.add('animate__animated', 'animate__jello');
+    element.addEventListener('animationend', handleAnimationEnd, { once: true });
+
+    function handleAnimationEnd(event) {
+      event.stopPropagation();
+      element.classList.remove('animate__animated', 'animate__jello');
+    }
+  }
 
 
   return (
@@ -19,6 +35,9 @@ function App() {
       <Navbar className="header" />
 
 
+
+      {/* this needs to be styled probably animation too*/}
+      <button className="animate__animated the_button" onClick={animate}>There are {count} coding club members!</button>
 
       <Navbar className="footer" />
     </>
