05dbc328188c2f46f7fba7cfd0b140a2cf22caf9
JeffreyDinackus <jeffreydinackus@proton.me>
2024-02-24 19:55:56 -0500

    built end componenet

--- src/End.jsx
+++ src/End.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg'
+import viteLogo from '/vite.svg';
+import 'bulma/css/bulma.min.css';
+import jsLogo from './assets/js.svg'
 
 function End() {
   return (
-    <><p>built with: </p><img src={reactLogo} alt="React Logo" /><img src={viteLogo} alt="React Logo" /></>)
+    <>
+
+
+      <div className="has-background-warning columns is-centered">
+        <div className="column is-one-third mt-4">
+          <p className="has-text-centered has-text-dark">built with: </p>
+          <div className="is-flex is-justify-content-center"> {/* Center the images */}
+            <img className="image is-32x32" src={reactLogo} alt="React Logo" /> {/* Use Bulma 'image' class */}
+            <img className="image is-32x32" src={jsLogo} alt="JS Logo" />
+
+            <img className="image is-32x32" src={viteLogo} alt="Vite Logo" />
+
+          </div>
+        </div>
+      </div>
+
+
+    </>)
 }
 
 export default End
