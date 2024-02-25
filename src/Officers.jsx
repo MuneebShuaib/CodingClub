c839edc44ef69ba55bcaf4b7afbde6b72948e4e7
JeffreyDinackus <jeffreydinackus@proton.me>
2024-02-25 16:46:57 -0500

    added pics and linkedins for officers

--- src/Officers.jsx
+++ src/Officers.jsx
@@ -1,17 +1,24 @@
 import React from 'react'
 import 'bulma/css/bulma.min.css';
+import diego from './assets/diego.jpg';
+import './App.css'
+import alait from './assets/alait.jpeg'
+import jeff from './assets/jeff.jpeg'
+
 
 function Officers() {
 
 
   const posts = [
-    { id: 1, name: 'Muneeb Shuaib', role: 'Current President', content: 'Muneeb primarily codes full stack with MERN. He was the first president to bring the Coding Club to in person meetings.', linkedin: '', github: '' },
-    { id: 2, name: 'Hazrat', role: 'Current Vice President', content: 'Hazrat is really good at cybersecurity and works in tech as technical support. He recently earned his CCNA.', linkedin: '', github: '' },
-    { id: 3, name: 'Diego', role: 'Current Treasurer', content: 'Diego is a low level programmer in his first semester at NVCC. He also works at a startup and contributes to open source.', linkedin: '', github: '' },
-    { id: 4, name: 'Alait', role: 'Curent Secretary', content: '', linkedin: '', github: '' },
+    { id: 1, name: 'Muneeb Shuaib', role: 'Current President', content: 'Muneeb primarily codes full stack with MERN. He was the first president to bring the Coding Club to in person meetings.', linkedin: 'https://www.linkedin.com/in/muneeb-shuaib-9a1b171b4/', github: 'https://github.com/MuneebShuaib', img: '', alt: '' },
+    { id: 2, name: 'Hazrat Alkozai', role: 'Current Vice President', content: 'Hazrat is really good at cybersecurity and works in tech as technical support. He recently earned his CCNA.', linkedin: 'https://www.linkedin.com/in/hazratalkozai/', github: '', img: '', alt: '' },
+    { id: 3, name: 'Diego', role: 'Current Treasurer', content: 'Diego is a low level programmer who also programs in haxe. He also works at a startup and contributes to open source.', linkedin: 'https://www.linkedin.com/in/diego-a-fonseca/', github: '', img: diego, alt: 'Diego\'s picture' },
+    {
+      id: 4, name: 'Alait Mesfune', role: 'Curent Secretary', content: 'Alait is a beginner programmer and the secretary for the club, he mainly handle the communications and announcements to members.', linkedin: 'https://www.linkedin.com/in/alait-mesfune-9901b726b/', github: 'https://github.com/tiala32', img: alait, alt: 'Alait\'s Picture'
+    },
 
-    { id: 5, name: 'Jeffrey Dinackus', role: 'Former President', content: 'Jeffrey was the president for Summer 2023, he since graduated NVCC and got a job as a systems administrator. He\'s also the person who built the website you are current browsing.', linkedin: 'https://www.linkedin.com/in/jeffdinackus/', github: 'https://github.com/jeffreydinackus' },
-    { id: 6, name: 'Aren', role: 'Former President', content: 'Aren has graduated NVCC and now goes to Virginia Tech where he is close to graduating. He is still active in the club through discord.', linkedin: '', github: '' },
+    { id: 5, name: 'Jeffrey Dinackus', role: 'Former President', content: 'Jeffrey was the president for Summer 2023, he since graduated NVCC and got a job as a systems administrator. He\'s also the person who built the website you are current browsing.', linkedin: 'https://www.linkedin.com/in/jeffdinackus/', github: 'https://github.com/jeffreydinackus', img: jeff, alt: 'Jeff\s Pic' },
+    { id: 6, name: 'Aren', role: 'Former President', content: 'Aren has graduated NVCC and now goes to Virginia Tech where he is close to graduating. He is still active in the club through discord.', linkedin: '', github: '', img: '', alt: '' },
 
 
 
@@ -23,12 +30,13 @@ function Officers() {
 
 
 
-  const listItems = posts.map(a => <div key={a.id}><h1 className="is-size-2" key={a.id}>{a.name}</h1>
+  const listItems = posts.map(a => <div key={a.id}><h1 className="is-size-2">{a.name}</h1>
+    <p key={`role_${a.id}`} className='is-size-4'>{a.role}</p>
 
-    <p key={a.id} className='is-size-4'>{a.role}</p>
-    <p key={a.id} className='is-size-4'>{a.content}</p>
-    <p key={a.id} className='is-size-4'><a href={a.linkedin}>{a.linkedin}</a></p>
-    <p key={a.id} className='is-size-4'><a href={a.github}>{a.github}</a></p>
+    <img src={a.img} className='image img-officer' alt={a.alt} />
+    <p key={`content_${a.id}`} className='is-size-4'>{a.content}</p>
+    <p key={`linkedin_${a.id}`} className='is-size-4'><a href={a.linkedin}>{a.linkedin}</a></p>
+    <p key={`github_${a.id}`} className='is-size-4'><a href={a.github}>{a.github}</a></p>
 
 
 
