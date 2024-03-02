866f75958b88f2ba761ac6a0c15627d40579e3b0
JeffreyDinackus <jeffreydinackus@proton.me>
2024-03-01 22:55:05 -0500

    changing some deployment details

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/nvcc-coding-club.github.io/',
-  plugins: [react()],
+  base: './', // Corrected base path
+  plugins: [react()]
 })
