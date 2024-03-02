a7e49e816211d896cc98d8570cb7038c5bcb8e55
JeffreyDinackus <jeffreydinackus@proton.me>
2024-03-01 21:38:44 -0500

    changing build script

--- src/Officers.jsx
+++ src/Officers.jsx
@@ -12,7 +12,7 @@ function Officers() {
   const posts = [
     { id: 1, name: 'Muneeb Shuaib', role: 'Current President', content: 'Muneeb primarily codes full stack with MERN. He was the first president to bring the Coding Club to in person meetings.', linkedin: 'https://www.linkedin.com/in/muneeb-shuaib-9a1b171b4/', github: 'https://github.com/MuneebShuaib', img: '', alt: '' },
     { id: 2, name: 'Hazrat Alkozai', role: 'Current Vice President', content: 'Hazrat is really good at cybersecurity and works in tech as technical support. He recently earned his CCNA.', linkedin: 'https://www.linkedin.com/in/hazratalkozai/', github: '', img: '', alt: '' },
-    { id: 3, name: 'Diego', role: 'Current Treasurer', content: 'Diego is a low level programmer who also programs in haxe. He also works at a startup and contributes to open source.', linkedin: 'https://www.linkedin.com/in/diego-a-fonseca/', github: 'https://github.com/Just-Feeshy', img: diego, alt: 'Diego\'s picture' },
+    { id: 3, name: 'Diego Fonseca', role: 'Current Treasurer', content: 'Diego is a low level programmer who also programs in haxe. He also works at a startup and contributes to open source.', linkedin: 'https://www.linkedin.com/in/diego-a-fonseca/', github: 'https://github.com/Just-Feeshy', img: diego, alt: 'Diego\'s picture' },
     {
       id: 4, name: 'Alait Mesfune', role: 'Curent Secretary', content: 'Alait is a beginner programmer and the secretary for the club, he mainly handle the communications and announcements to members.', linkedin: 'https://www.linkedin.com/in/alait-mesfune-9901b726b/', github: 'https://github.com/tiala32', img: alait, alt: 'Alait\'s Picture'
     },
